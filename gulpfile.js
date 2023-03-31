17b7d0482f8d9aa670517012a157b788df8caa86
Thiago Alô <thiago_kadri@hotmail.com>
2023-03-31 22:09:11 +0100

    add html-minifier, http server, babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,104 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat')
-const cssmin = require('gulp-cssmin')
-const rename = require('gulp-rename')
-const uglify = require('gulp-uglify')
-const image = require('gulp-imagemin')
-const stripJs = require('gulp-strip-comments')
-const stripCss = require('gulp-strip-css-comments')
+const { series, parallel } = require('gulp');
+const babel = require('gulp-babel');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const stripJs = require('gulp-strip-comments');
+const stripCss = require('gulp-strip-css-comments');
+const htmlmin = require('html-minifier');
+mime = require('mime');
 
 function tarefasCSS(cb) {
-
-    return gulp.src([
+    gulp.src([
             './node_modules/bootstrap/dist/css/bootstrap.css',
             './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
             './vendor/owl/css/owl.css',
             './vendor/jquery-ui/jquery-ui.css',
-            './src/css/style.css'
+            './src/css/style.css',
         ])
-        .pipe(stripCss())                   // remove comentários
-        .pipe(concat('styles.css'))         // mescla arquivos
-        .pipe(cssmin())                     // minifica css
-        .pipe(rename({ suffix: '.min'}))    // styles.min.css
-        .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
-
+        .pipe(stripCss()) // remove comentários css
+        .pipe(concat('styles.css')) // mescla arquivos
+        .pipe(cssmin()) // minifica css
+        .pipe(rename({ suffix: '.min' })) // styles.min.css
+        .pipe(gulp.dest('./dist/css')) // cria arquivo em novo diretório
+        .on('end', cb); // informa que a tarefa está concluída
 }
 
-function tarefasJS(){
-
-    return gulp.src([
+function tarefasJS() {
+    return gulp
+        .src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             './vendor/owl/js/owl.js',
             './vendor/jquery-mask/jquery.mask.js',
             './vendor/jquery-ui/jquery-ui.js',
-            './src/js/custom.js'
+            './src/js/custom.js',
         ])
-        .pipe(stripJs())                    // remove comentários
-        .pipe(concat('scripts.js'))         // mescla arquivos
-        .pipe(uglify())                     // minifica js
-        .pipe(rename({ suffix: '.min'}))    // scripts.min.js
-        .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
+        .pipe(stripJs()) // remove comentários
+        .pipe(concat('scripts.js')) // mescla arquivos
+        .pipe(uglify()) // minifica js
+        .pipe(rename({ suffix: '.min' })) // scripts.min.js
+        .pipe(gulp.dest('./dist/js')); // cria arquivo em novo diretório
 }
 
-
-function tarefasImagem(){
-    
-    return gulp.src('./src/images/*')
-        .pipe(image({
-            pngquant: true,
-            optipng: false,
-            zopflipng: true,
-            jpegRecompress: false,
-            mozjpeg: true,
-            gifsicle: true,
-            svgo: true,
-            concurrent: 10,
-            quiet: true
-        }))
-        .pipe(gulp.dest('./dist/images'))
+function tarefasImagem() {
+    return import('gulp-image').then((image) => {
+        return gulp
+            .src('./src/images/*')
+            .pipe(
+                image.default({
+                    pngquant: true,
+                    optipng: false,
+                    zopflipng: true,
+                    jpegRecompress: false,
+                    mozjpeg: true,
+                    gifsicle: true,
+                    svgo: true,
+                    concurrent: 10,
+                    quiet: true,
+                })
+            )
+            .pipe(gulp.dest('./dist/images'));
+    });
 }
+
+// POC - Proof of Concept
+gulp.task('minify-html', () => {
+    return gulp
+        .src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
+
+function tarefasHTML() {
+    return gulp.src('src/*.html').pipe(gulp.dest('dist'));
+}
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+            mime: {
+                'text/javascript': ['js']
+            }
+        },
+    });
+    gulp.watch('./src/**/*').on('change', process); // repete o processo quando alterar algo em src
+    gulp.watch('./src/**/*').on('change', reload);
+});
+
+function end(cb) {
+    console.log('tarefas concluídas');
+    return cb();
+}
+
+// series x parallel
+const process = parallel( tarefasHTML, tarefasJS, tarefasCSS, end)
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = process
